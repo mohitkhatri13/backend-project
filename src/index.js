323e77cbdcdaba31fb9f39210fdcb0ccabd88790
Mohit Khatri <khatrimohit2002@gmail.com>
2024-03-29 00:02:22 +0530

    k

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ dotenv.config({
 })
 
 connectDB()
-
+.then( ()=>{
+      app.listen(process.env.PORT ||4000,()=>{
+            console.log(`Server is running at port :${process.env.PORT}`);
+      } ) 
+})
+.catch((err)=>{
+    console.log("mongo db connection failed !!!" , err);
+})
 
 
 
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,25 @@
+
+//these are two methods of utility async function
+
+const asynchandler = (requestHandler)=>{
+     (req , res , next)=>{
+         Promise.resolve(requestHandler(req , res, next))
+         .catch((err)=>next(err))
+     }
+}
+
+// const asynchandler = ()=>{}
+// const asynchandler = (func)=>()=>{}
+//const asynchandler = (func)=>async()=>{}
+// const asynchandler  = (fn)=>async (req , res , next)=> {
+//       try{
+//               await fn(req , res , next)
+//       }
+//       catch(error){
+//          res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//          })
+//       }
+// }
+export default asynchandler
