c9e9611b30fbb4eb52cbb165d40d4a78645d5d92
Mohit Khatri <khatrimohit2002@gmail.com>
2024-03-29 13:22:46 +0530

    hello

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -58,11 +58,38 @@ userSchema.pre("save" ,async function (next) {
 
     this.password = bcrypt.hash(this.password ,10)
     next()
-})
+}) //here is the problem means jab bhi data me kuch change hoga toh ye function call hoga 
+// so we want that it  runs only when we change our password or save it  so we use if consition inside it 
 
+
+//this is custom method created in middleware  
 userSchema.methods.isPasswordCorrect = async function 
 (password){
    return await bcrypt.compare(password , this.password)
+}
+userSchema.methods.generateAccessToken = fundtion(){
+  return jwt.sign({
+     _id:this._id,
+     email:this.email,
+     username:this.username,
+     fullname:this.fullname
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+     expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+  )
+}
+userSchema.methods.generateRefreshToken = fundtion(){
+  return jwt.sign({
+    _id:this._id,
+ },
+ process.env.REFRESH_TOKEN_SECRET,
+ {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+ }
+ )
+
 }
 
 
